d170858952b0e4ac48a13000471e2975824f5536
mcandar <mccandar@gmail.com>
2017-04-24 11:16:34 +0300

    Update appr.R

--- appr.R
+++ appr.R
@@ -20,7 +20,9 @@ if(!("devtools" %in% rownames(installed.packages()))) install.packages("devtools
 if(!("dygraphs" %in% rownames(installed.packages()))) install.packages("dygraphs")
 if(!("nnet" %in% rownames(installed.packages()))) install.packages("nnet")
 if(!("neuralnet" %in% rownames(installed.packages()))) install.packages("neuralnet")
-if(!("geosphere" %in% rownames(installed.packages()))) install.packages("geosphere")  
+if(!("geosphere" %in% rownames(installed.packages()))) install.packages("geosphere")
+if(!("dtw" %in% rownames(installed.packages()))) install.packages("dtw")
+if(!("highcharter" %in% rownames(installed.packages()))) install.packages("highcharter")
 if(!("h2o" %in% rownames(installed.packages()))) install.packages("h2o") # requires source files, www.h2o.ai/download/
 
 # Get zip information from github, for mapping purposes

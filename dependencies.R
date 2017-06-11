c64aec9c23b0e00724c6eda9c24b2cb13eafa6f0
mcandar <mccandar@gmail.com>
2017-06-11 23:16:32 +0300

    Create dependencies.R

---
+++ dependencies.R
@@ -0,0 +1,14 @@
+### COMMON DEPENDENCIES ###
+
+if(!("plotly" %in% rownames(installed.packages()))) install.packages("plotly")
+if(!("lubridate" %in% rownames(installed.packages()))) install.packages("lubridate")
+if(!("htmlwidgets" %in% rownames(installed.packages()))) install.packages("htmlwidgets")
+if(!("htmltools" %in% rownames(installed.packages()))) install.packages("htmltools")
+if(!("devtools" %in% rownames(installed.packages()))) install.packages("devtools")
+if(!("dygraphs" %in% rownames(installed.packages()))) install.packages("dygraphs")
+if(!("nnet" %in% rownames(installed.packages()))) install.packages("nnet")
+if(!("neuralnet" %in% rownames(installed.packages()))) install.packages("neuralnet")
+if(!("geosphere" %in% rownames(installed.packages()))) install.packages("geosphere")
+if(!("dtw" %in% rownames(installed.packages()))) install.packages("dtw")
+if(!("highcharter" %in% rownames(installed.packages()))) install.packages("highcharter")
+if(!("xlsx" %in% rownames(installed.packages()))) install.packages("xlsx")
